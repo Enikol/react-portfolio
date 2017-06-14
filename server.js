8091d77508bded68b3ccda3d3f9beed87abfe861
Catherine <enikolaenko330@gmail.com>
2017-06-15 02:38:27 +0300

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('*', function (req, res) {
-  res.sendFile('/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 /*app.get('/resume', function(req, res){
 
