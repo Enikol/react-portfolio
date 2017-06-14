1de798b62b4a9f1e28bee2cfd9df6eca5e919286
Catherine <enikolaenko330@gmail.com>
2017-06-15 02:55:13 +0300

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,9 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+  plugins: [
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+})
+  ]
 };
